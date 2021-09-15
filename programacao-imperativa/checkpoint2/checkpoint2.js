8d6b579f114083df90522985e3ab04ee6b7c82f9
Matheus <Matheus@DESKTOP-8FDT956>
2021-09-14 22:28:16 -0300

    checkpoint PImperativa

---
+++ programacao-imperativa/checkpoint2/checkpoint2.js
@@ -0,0 +1,63 @@
+// function soma(a=1, b=3){
+//     return a+b;
+// }
+
+// console.log(soma());
+// console.log(soma(3));
+// console.log(soma(1,2));
+
+// let numeros= [10, 8, 7, 99, 0, 1, 6]
+// numeros.sort(function(a,b){return b-a});
+// console.log(numeros)
+
+
+// var z=0;
+// for (var i=20; i<50; i+=10){
+//     z+=i;
+// }
+// console.log(z);
+
+// let frutasAmarelas = ['melao', 'mamao', 'limao siciliano'];
+// let frutasVermelhas = ['morango', 'cereja', 'maçã'];
+// let frutasVerdes = ['limão', 'kiwi', 'maçã verde'];
+
+// frutasVermelhas1 = frutasVermelhas.map(fruta => fruta.toUpperCase())
+
+// // let todasFrutas = [...frutasAmarelas,...frutasVermelhas1,...frutasVerdes ]
+// let todasFrutas = [frutasAmarelas,frutasVermelhas1,frutasVerdes ]
+// console.log(todasFrutas)
+
+// let valor = 8;
+// let fatorial = 1;
+
+// for(let i = valor; i > 1; i--){
+//     fatorial *= i;
+// }
+
+// console.log(`Fatorial de ${valor} = ${fatorial}`);
+
+// let frutasAmarelas = ['melao', 'mamao', 'limao siciliano'];
+// let [fruta1, fruta2, fruta3] = frutasAmarelas;
+
+// console.log(fruta1);
+// console.log(fruta2);
+// console.log(fruta3);
+// console.log(frutasAmarelas);
+
+// let x = 5;
+// let y = x++ + ++x;
+// console.log("y= "+y);
+
+// x=3;
+// y = x*(x+1)*x++;
+// x=5;
+// y=3;
+// y*= x+1;
+// console.log("x= "+x);
+// console.log("y= "+y);
+
+for (var i=0; i<50; i+=10){
+    console.log(i);
+}
+
+console.log(i);
